5a9c3ba49c31b5094f7f1bf8d0aaaf39c03320f7
khusnadwi7-beep <khusnadwi7@gmail.com>
2026-06-09 21:59:14 +0700

    Update config.js

--- sheikoshop/config.js
+++ sheikoshop/config.js
@@ -1,5 +1,2 @@
-window.SUPABASE_URL =
-"https://rfluidvgdrtxiidibivt.supabase.co";
-
-window.SUPABASE_ANON_KEY =
-"sb_publishable_YsXecJLH_QopgQ6SHAS-DQ_N926dQul";
+window.SUPABASE_URL = "https://rfluidvgdrtxiidibivt.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_YsXecJLH_QopgQ6SHAS-DQ_N926dQul";
